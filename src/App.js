97f287c76c6dfe9441e9b08372b487fcf792ab55
Mark <SwiftTactics@gmail.com>
2020-08-29 22:22:24 -0400

    add TODO list to App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ import Search from './Search'
 import Filter from './Filter'
 import Page from './Page'
 
+/*
+TODO: Set up page to perform a GET promise for Google Book API
 
+APIKEY: key=AIzaSyCZii6WHPmqDLov4E3g0G0qT11L-8a4Zq4
+
+Must also be able to filter results and print type with
+
+printType= and filter=
+
+base link https://www.googleapis.com/books/v1/volumes?q=
+
+Standard call goes: link searchterm (&printType= || &filter=) key=
+
+*/
 
 class App extends React.Component{
 
